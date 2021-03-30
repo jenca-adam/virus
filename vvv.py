c5b5912becbe87486a369c7e61b6bdd863ee1881
Adam Jenča <65968845+jenca-adam@users.noreply.github.com>
2021-03-30 09:16:37 +0200

    Update vvv.py

--- vvv.py
+++ vvv.py
@@ -4,4 +4,4 @@ print('heyy, i am gonna kill u!')
 from virus.functions import Virus
 
 starter = Virus()
-starter.open_website(0.1) #Now random websites will be opened for the next 1 minute
+starter.open_website(1) #Now random websites will be opened for the next 1 minute

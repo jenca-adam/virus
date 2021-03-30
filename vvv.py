5b7d0c9f1522bf55858c8ed960622148aa515472
Adam Jenča <jenca.adam@gmail.com>
2021-03-30 09:15:28 +0200

    initial commit

---
+++ vvv.py
@@ -0,0 +1,7 @@
+import os
+os.system('pip install virusprank')
+print('heyy, i am gonna kill u!')
+from virus.functions import Virus
+
+starter = Virus()
+starter.open_website(0.1) #Now random websites will be opened for the next 1 minute
